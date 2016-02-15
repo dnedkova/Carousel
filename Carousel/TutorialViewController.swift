eb5ea363dd0b18c82362d5a8466a10b5f4487979
Diana Nedkova <dnedkova@lyft.com>
2016-02-15 13:00:47 -0800

    add animation

--- Carousel/TutorialViewController.swift
+++ Carousel/TutorialViewController.swift
@@ -14,18 +14,30 @@ class TutorialViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var pageControl: UIPageControl!
     
+    @IBOutlet weak var backUpButton: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         tutorialScrollView.contentSize = CGSize(width: 1280, height: 568)
         tutorialScrollView.delegate = self
+        backUpButton.alpha = 0
     }
     
     func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
         let page: Int = Int(round(tutorialScrollView.contentOffset.x / 320))
         
         pageControl.currentPage = page
+        
+        if page == 3 {
+            pageControl.hidden = true
+            UIView.animateWithDuration(0.4, animations: {
+                self.backUpButton.alpha = 1
+                self.pageControl.hidden = true
+                
+            })
+        }
+            
     }
     
     override func didReceiveMemoryWarning() {
